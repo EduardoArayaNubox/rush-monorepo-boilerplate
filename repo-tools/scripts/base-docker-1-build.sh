d3a292e9b1ee6ea567ed2a35aa9be5d60e677f6b
Matthew Gabeler-Lee <mgabeler-lee@6river.com>
2019-05-21 23:46:22 -0400

    fix: optimizing ci flow

--- repo-tools/scripts/base-docker-1-build.sh
+++ repo-tools/scripts/base-docker-1-build.sh
@@ -4,11 +4,19 @@ set -euo pipefail
 set -x
 
 shasum="$(cat common/config/rush/pnpm-lock.yaml rush_cache_1.dockerfile | sha1sum | cut -d' ' -f1)"
+# have to put the flag file in a constantly named directory so that circle caching can pick it up
+flagdir="rush_cache_1.built"
+flagfile="${flagdir}/rush_cache_1_${shasum}.built"
 
-if ! docker pull "gcr.io/plasma-column-128721/${CIRCLE_PROJECT_REPONAME}_rush_cache_1_${shasum}" ; then
+if [ -f "${flagfile}" ]; then
+	echo "Found flag file, assuming rush_cache_1 is already built for ${sha1sum}"
+elif ! docker pull "gcr.io/plasma-column-128721/${CIRCLE_PROJECT_REPONAME}_rush_cache_1_${shasum}" ; then
+	# still needed to do the pull check in case cache was built before flag file caching was added
   docker build -t rush_cache_1 -f rush_cache_1.dockerfile .
   docker tag rush_cache_1 "gcr.io/plasma-column-128721/${CIRCLE_PROJECT_REPONAME}_rush_cache_1_${shasum}"
   docker push "gcr.io/plasma-column-128721/${CIRCLE_PROJECT_REPONAME}_rush_cache_1_${shasum}"
+	mkdir -p "$flagdir"
+	touch "$flagfile"
 else
   echo "tagging rush_cache docker tag gcr.io/plasma-column-128721/rush_cache_1_$shasum rush_cache"
   docker tag "gcr.io/plasma-column-128721/${CIRCLE_PROJECT_REPONAME}_rush_cache_1_${shasum}" rush_cache_1
--- repo-tools/scripts/base-docker-2-build.sh
+++ repo-tools/scripts/base-docker-2-build.sh
@@ -6,6 +6,6 @@ set -x
 # this is separate from prep just to get better timing information out of CI
 
 # Ignore this big directory because we don't need it for the next step
-echo "common/temp/node_modules" >> .dockerignore
+echo "common/temp" >> .dockerignore
 
 docker build -t rush_cache -f rush_cache_2.dockerfile .
