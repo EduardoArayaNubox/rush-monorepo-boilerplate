0d51e2730ab979b8bbd32d160bdbe6e2dd85c26f
Matthew Gabeler-Lee <mgabeler-lee@6river.com>
2019-05-21 23:21:25 -0400

    fix: typo

--- repo-tools/scripts/base-docker-1-pull.sh
+++ repo-tools/scripts/base-docker-1-pull.sh
@@ -3,9 +3,10 @@
 set -euo pipefail
 set -x
 
-shasum="$(shasum /tmp/cache_check.txt | awk '{print $1}')"
+shasum="$(cat common/config/rush/pnpm-lock.yaml rush_cache_1.dockerfile | sha1sum | cut -d' ' -f1)"
 
 # TODO: is it faster to pull this image from gcr.io, or to use docker save/load
+# TODO: all we actually care about is if the image exists, but that's hard to find out without pulling
 if ! docker pull "gcr.io/plasma-column-128721/${CIRCLE_PROJECT_REPONAME}_rush_cache_1_${shasum}" ; then
   echo "rush_cache_1 was lost from prior step?!" 1>&2
 	exit 1
