62d6fb27c19ed403a92ff88a9ca512502d4313ff
Matthew Gabeler-Lee <mgabeler-lee@6river.com>
2019-05-22 01:21:53 -0400

    fix: typo

--- repo-tools/scripts/base-docker-2-build.sh
+++ repo-tools/scripts/base-docker-2-build.sh
@@ -8,7 +8,7 @@ echo "common/temp" >> .dockerignore
 
 docker build -t rush_cache -f rush_cache_2.dockerfile .
 # get the full id of the just-built image to ues as a tag
-imageid=$(docker images -q --no-trunc | cut -d: -f2)
+imageid=$(docker images -q --no-trunc rush_cache | cut -d: -f2)
 # make sure we got something plausibly long
 test "${#imageid}" -gt 10
 docker tag rush_cache "gcr.io/plasma-column-128721/${CIRCLE_PROJECT_REPONAME}_rush_cache_2_${imageid}"
