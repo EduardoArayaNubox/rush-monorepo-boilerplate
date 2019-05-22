296882f161f1884077a6c053aa4eb4cc9481139e
Matthew Gabeler-Lee <mgabeler-lee@6river.com>
2019-05-21 23:01:42 -0400

    fix: better docker parallelism

--- repo-tools/scripts/create-base-docker-2-build.sh
+++ repo-tools/scripts/create-base-docker-2-build.sh
@@ -2,4 +2,7 @@
 
 # this is separate from prep just to get better timing information out of CI
 
+# Ignore this big directory because we don't need it for the next step
+echo "common/temp/node_modules" >> .dockerignore
+
 docker build -t rush_cache -f rush_cache_2.dockerfile .
