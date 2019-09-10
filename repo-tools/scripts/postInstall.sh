808f7d7fbc6f38c8ce26a879bf0da934f9894562
Matthew Gabeler-Lee <mgabeler-lee@6river.com>
2019-09-10 13:13:54 -0400

    fix: remove post install script that doesn't work and does nothing

--- repo-tools/scripts/postInstall.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-PACKAGES=$(find $(pwd -P) -name package.json -type f -not -path "*/node_modules/*" -not -path "*/rush-recycler/*")
-
-for p in $PACKAGES; do
-	# TODO: What do we need to do here?
-	true
-done
