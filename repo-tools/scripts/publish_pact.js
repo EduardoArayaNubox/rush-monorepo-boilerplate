f5f815c74d9bbc96b4af9cf76c78812fa1b141da
Matthew Gabeler-Lee <mgabeler-lee@6river.com>
2019-10-02 22:04:38 -0400

    fix: better way to allow empty pacts output

--- repo-tools/scripts/publish_pact.js
+++ repo-tools/scripts/publish_pact.js
@@ -1,5 +1,19 @@
+const fs = require('fs');
 const pact = require('@pact-foundation/pact-node');
 const package = require(`${process.env.PWD}/package.json`);
+
+const pactsPath = './pacts';
+
+// stderr output will fail the build, gracefully skip this if there are no pacts to publish
+if (process.argv.some((arg) => arg == '--allow-empty')) {
+	if (!fs.existsSync(pactsPath)) {
+		console.log('No pacts dir found');
+		process.exit(0);
+	} else if (!fs.readdirSync(pactsPath).length) {
+		console.log('No pacts found in dir');
+		process.exit(0);
+	}
+}
 
 (async () => {
 	await pact.publishPacts({
