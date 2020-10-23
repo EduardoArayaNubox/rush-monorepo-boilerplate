b6fdf3cd1e01ec36cb4dbffbac699b08da42f971
Matthew Gabeler-Lee <mgabeler-lee@6river.com>
2020-10-23 17:17:49 -0400

    fix: use env bash for future proofing
    Co-authored-by: Tony Gaetani <tony.gaetani@gmail.com>

--- packages/service/scripts/prod-init.sh
+++ packages/service/scripts/prod-init.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # This script is to be run as the init container in production,
 # to allow lengthy database migrations to have a longer timeout than normal app startup
