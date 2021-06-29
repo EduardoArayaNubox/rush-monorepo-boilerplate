2751314f7f9282ba311677c051fd3859d7abbb82
Spencer Goossens <sgoossens@6river.com>
2021-06-29 16:51:40 -0400

    fix: updates oas target

--- repo-tools/scripts/synchronize-yaml.sh
+++ repo-tools/scripts/synchronize-yaml.sh
@@ -1,8 +1,9 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+DO_IT=${1:-false}
 
 # Get the root path of the repo
 ROOT=$(git rev-parse --show-toplevel)
-echo "Found repo root: ${ROOT}"
 
 # Get the version from package.json
 PACKAGE_VERSION=$(cat ${ROOT}/packages/*/package.json \
@@ -11,19 +12,19 @@ PACKAGE_VERSION=$(cat ${ROOT}/packages/*/package.json \
   | awk -F: '{ print $2 }' \
   | sed 's/[",]//g' \
   | tr -d '[[:space:]]')
-echo "Extracted version: ${PACKAGE_VERSION}"
 
 # Find the yaml files in packages/oas tht are committed to git
-YAML_FILES=$(git ls-files ${ROOT}/packages/oas/ \
-  | xargs -I {} find {} -type f -maxdepth 3 -name "*.yaml")
+YAML_FILES=$(git ls-files --full-name "${ROOT}/packages/oas/*.yaml" "${ROOT}/packages/oas/**/*.yaml")
 
 # Now do the replacement in-place (MacOS/Unix compatible) https://frontend-apps.6river.org/api/v${PACKAGE_VERSION}
-for FILE in ${YAML_FILES}
-do
+for FILE in ${YAML_FILES} ; do
   # If the yaml file imports from standard api, update the version
-  if grep -q "https://frontend-apps.6river.org/api/v" ${ROOT}/${FILE}
-  then
-    sed -i.bak -e "s#\(https://frontend-apps.6river.org/api/v\)[^/]*/#https://frontend-apps.6river.org/api/v${PACKAGE_VERSION}/#g" "${ROOT}/${FILE}"
-    rm ${ROOT}/${FILE}.bak
+  if grep -q "https://frontend-apps.6river.org/api/v" "${ROOT}/${FILE}" ; then
+    if [[ ${DO_IT} = true ]] ; then
+      sed -i.bak -e "s#\(https://frontend-apps.6river.org/api/v\)[^/]*/#https://frontend-apps.6river.org/api/v${PACKAGE_VERSION}/#g" "${ROOT}/${FILE}"
+      rm "${ROOT}/${FILE}.bak"
+    else
+      diff "${ROOT}/${FILE}" <( sed -e "s#\(https://frontend-apps.6river.org/api/v\)[^/]*/#https://frontend-apps.6river.org/api/v${PACKAGE_VERSION}/#g" "${ROOT}/${FILE}" )
+    fi
   fi
 done
