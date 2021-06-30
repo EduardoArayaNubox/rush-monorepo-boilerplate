d04b3122d6c62a60ad01f926ea1d22c5e4c30c29
Spencer Goossens <sgoossens@6river.com>
2021-06-30 09:48:39 -0400

    fix: sets a default package version if not found

--- repo-tools/scripts/synchronize-yaml.sh
+++ repo-tools/scripts/synchronize-yaml.sh
@@ -1,12 +1,18 @@
 #!/usr/bin/env bash
 
 DO_IT=${1:-false}
+PACKAGE_VERSION_DEFAULT="2.x.x"
 
 # Get the root path of the repo
 ROOT=$(git rev-parse --show-toplevel)
 
 # Get the version from package.json
 PACKAGE_VERSION=$(jq -r '.dependencies["@sixriver/standard-api"] | select(. != null)' ${ROOT}/packages/*/package.json | head -1)
+
+# If there is no value found, default the version to what is in the monorepo-boilerplate
+if [ -z "${PACKAGE_VERSION}" ] ; then
+  PACKAGE_VERSION=${PACKAGE_VERSION_DEFAULT}
+fi
 
 # Find the yaml files in packages/oas that are committed to git
 YAML_FILES=$(git ls-files --full-name "${ROOT}/packages/oas/*.yaml" "${ROOT}/packages/oas/**/*.yaml")
