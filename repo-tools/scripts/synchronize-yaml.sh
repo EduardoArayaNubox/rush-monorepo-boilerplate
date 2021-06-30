0d1ad5599acab7603daf100924140f2ae43598eb
Spencer Goossens <sgoossens@6river.com>
2021-06-30 09:29:18 -0400

    fix: refactor comments

--- repo-tools/scripts/synchronize-yaml.sh
+++ repo-tools/scripts/synchronize-yaml.sh
@@ -8,10 +8,10 @@ ROOT=$(git rev-parse --show-toplevel)
 # Get the version from package.json
 PACKAGE_VERSION=$(jq -r '.dependencies["@sixriver/standard-api"] | select(. != null)' ${ROOT}/packages/*/package.json | head -1)
 
-# Find the yaml files in packages/oas tht are committed to git
+# Find the yaml files in packages/oas that are committed to git
 YAML_FILES=$(git ls-files --full-name "${ROOT}/packages/oas/*.yaml" "${ROOT}/packages/oas/**/*.yaml")
 
-# Now do the replacement in-place (MacOS/Unix compatible) https://frontend-apps.6river.org/api/v${PACKAGE_VERSION}
+# Do an in place replacement of the version
 for FILE in ${YAML_FILES} ; do
   # If the yaml file imports from standard api, update the version
   if grep -q "https://frontend-apps.6river.org/api/v" "${ROOT}/${FILE}" ; then
